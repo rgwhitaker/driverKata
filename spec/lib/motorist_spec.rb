c52c2ff2d3b87b75203a3007bde113cf13b74908
Rob Whitaker <rob.whitaker.dev@gmail.com>
2018-08-08 17:53:15 -0400

    added more expectations for the motorist spec moved the motorist object into an instance scoped object

--- spec/lib/motorist_spec.rb
+++ spec/lib/motorist_spec.rb
@@ -2,20 +2,36 @@ require_relative '../spec_helper'
 require_relative '../../lib/motorist'
 
 describe 'Motorist' do
+  before :all do
+    @motorist = Motorist.new('Rob')
+  end
 
   it 'does not have an empty name' do
-    motorist = Motorist.new('Rob')
-    expect(motorist.name).not_to be_empty
+    expect(@motorist.name).not_to be_empty
   end
 
   it 'is named Rob' do
-    motorist = Motorist.new('Rob')
-    expect(motorist.name).to eq 'Rob'
+    expect(@motorist.name).to eq 'Rob'
   end
 
   it 'starts with 0 miles driven' do
-    motorist = Motorist.new('Rob')
-    expect(motorist.milesDriven).to eq 0
+    expect(@motorist.milesDriven).to eq 0
   end
 
+  it 'can have its miles added to' do
+    @motorist.updateMilesDriven 21
+    expect(@motorist.milesDriven).to eq(21)
+  end
+
+  it 'can have its miles added twice' do
+    @motorist.updateMilesDriven 39
+    expect(@motorist.milesDriven).to eq(60)
+  end
+
+  it 'will round miles added' do
+    @motorist.updateMilesDriven 4.5
+    expect(@motorist.milesDriven).to eq(65)
+    @motorist.updateMilesDriven 0.4
+    expect(@motorist.milesDriven).to eq(65)
+  end
 end
