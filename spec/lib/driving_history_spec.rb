b4b2d0ea44a4d2c5aef3b88ef55481af53389d10
Rob Whitaker <rob.whitaker.dev@gmail.com>
2018-08-08 20:07:31 -0400

    added example for driving history class

--- spec/lib/driving_history_spec.rb
+++ spec/lib/driving_history_spec.rb
@@ -1,3 +1,13 @@
-require '../spec_helper'
+require_relative '../spec_helper'
+require_relative '../../lib/driving_history'
 
-describe
+describe 'DrivingHistory' do
+  before :all do
+    @drivingHistory = DrivingHistory.new
+    ReadInput::readInput @drivingHistory
+  end
+
+  it 'will output the correct data' do
+    expect { @drivingHistory.calculateOutput }.to output("Alex: 42 miles @ 34 mph\nDan: 39 miles @ 50 mph\nBob: 0 miles\n").to_stdout
+  end
+end
